0346f5feb2a639aeb6f7edbd98e2530104aed886
justinjeanpierre <justin.jeanpierre@gmail.com>
2015-06-24 20:49:43 -0400

    Moved routes out of server.js and into routes.js.

--- server.js
+++ server.js
@@ -19,59 +19,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 app.use(methodOverride());
 
-// routes
-//  api
-
-app.get('/api/todos', function(req, res) {
-	Todo.find(function(err, todos) {
-		if (err) {
-			res.status(500).json({message:'error finding todos'});
-		}
-			
-		res.status(200).json(todos);
-	});
-});
-
-app.post('/api/todos', function(req, res) {
-	Todo.create({
-		text:req.body.text,
-		done: false
-	}, function(err, todo) {
-		if (err) {
-			res.status(500).json({message:'error creating todo'});
-		}
-		
-		Todo.find(function(err, todos) {
-			if (err) {
-				res.status(500).json({message:'error retrieving todos'});
-			}
-			
-			res.status(200).json(todos);
-		});
-	});
-});
-
-app.delete('/api/todos/:todo_id', function(req, res) {
-	Todo.remove({
-		_id:req.params.todo_id
-	}, function(err, todo) {
-		if (err)
-			res.status(500).json({message:'error deleting todo'});
-			
-		Todo.find(function(err, todos) {
-			if (err) {
-				res.status(500).json({message:'error retrieving todos'});
-			}
-			
-			res.status(200).json(todos);
-		});
-	});
-});
-
-// application
-app.get("*", function(req, res) {
-	res.sendfile('./public/index.html');
-});
+// load routes
+require('./app/routes')(app);
 
 // listen
 var port = 3431;
