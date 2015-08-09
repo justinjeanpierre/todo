4b9340a9cd6783e68c5cf387ed35b391af9b7f2e
justinjeanpierre <justin.jeanpierre@gmail.com>
2015-08-09 16:34:04 -0400

    Server will listen on the specified PORT number (default is 3431).
    Run `PORT=XXXX node app.js` to start.

---
+++ app.js
@@ -0,0 +1,29 @@
+// app.js
+
+// set up
+var express		= require('express');
+var app			= express();
+var mongoose	= require('mongoose');
+var morgan		= require('morgan');
+var bodyParser	= require('body-parser');
+var methodOverride	= require('method-override');
+var parseArgs = require('minimist');
+
+// configuration
+var database = require('./config/database');
+mongoose.connect(database.url);
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(methodOverride());
+
+// load routes
+require('./app/routes')(app);
+
+// listen
+var server = app.listen(process.env.PORT || 3431, function() {
+  console.log('todo listening on port ' + server.address().port);
+});
