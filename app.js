3895ccb4b62335e7eae6abb818b053eda15905ce
justinjeanpierre <justin.jeanpierre@gmail.com>
2015-08-09 16:36:22 -0400

    Removed minimist from package.json

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var mongoose	= require('mongoose');
 var morgan		= require('morgan');
 var bodyParser	= require('body-parser');
 var methodOverride	= require('method-override');
-var parseArgs = require('minimist');
 
 // configuration
 var database = require('./config/database');
