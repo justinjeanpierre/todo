4276c9db5347abeb5bc0b9fe3286e92b930dba4a
justinjeanpierre <justin.jeanpierre@gmail.com>
2016-09-13 17:51:49 -0400

    Added mLab u:p.

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 // config/database.js
 
 module.exports = {
-	url: 'mongodb://localhost:27017/todo'
+	// url: 'mongodb://localhost:27017/todo'
+	url: 'mongodb://th-todo:th-todo-pw@ds029496.mlab.com:29496/treehouse-todo'
 };
